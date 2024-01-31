42616772fa090eecab6448e9e27d37911ede577e
au210757 <120127920+au210757@users.noreply.github.com>
2024-01-30 21:54:14 -0500

    Basically the finished project for Program 1. Everything seems to be working ok.

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.tipcalculator;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Double percentage = 0.0;
+    Double subtotal = 0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tip();
+    }
+    protected void tip(){
+        Button fifteenPercentButton = findViewById(R.id.btnPercent15);
+        Button twentyPercentButton = findViewById(R.id.btnPercent20);
+        Button customPercentButton = findViewById(R.id.btnPercentCustom);
+        Button confirmCustomTipButton = findViewById(R.id.btnConfirm);
+
+        EditText subtotalText = findViewById(R.id.txtSubtotal);
+        EditText tipPercentage = findViewById(R.id.txtCustomPercent);
+
+        TextView totalLabel = findViewById(R.id.lblTotal);
+
+        LinearLayout tipEntryBar = findViewById(R.id.lytCustomTip);
+
+        tipEntryBar.setVisibility(View.INVISIBLE);
+
+        fifteenPercentButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                tipEntryBar.setVisibility(View.INVISIBLE);
+                subtotal = Double.parseDouble(subtotalText.getText().toString());
+                percentage = 0.15;
+                totalLabel.setText(String.format(Double.toString(calculate(subtotal, percentage))));
+            }
+        });
+
+        twentyPercentButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                tipEntryBar.setVisibility(View.INVISIBLE);
+                subtotal = Double.parseDouble(subtotalText.getText().toString());
+                percentage = .2;
+                totalLabel.setText(String.format(Double.toString(calculate(subtotal, percentage))));
+            }
+        });
+
+        customPercentButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                tipEntryBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        confirmCustomTipButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                subtotal = Double.parseDouble(subtotalText.getText().toString());
+                percentage = Double.parseDouble(tipPercentage.getText().toString()) / 100;
+                totalLabel.setText(String.format(Locale.US, "%.2f", calculate(subtotal, percentage)));
+            }
+        });
+
+
+
+
+    }
+
+    public double calculate(double subtotal, double tip){
+        return subtotal + (subtotal*tip);
     }
 }
+
+
